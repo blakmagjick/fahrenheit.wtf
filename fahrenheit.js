1471cc5a12148dcd62cda0dc6798d56829f8dbad
blakmagjick <suzy@iamsuzyq.com>
2022-04-24 13:16:58 -0700

    Fallback timeout may actually be messing up geolocation

--- fahrenheit.js
+++ fahrenheit.js
@@ -92,7 +92,7 @@ const getDefaultLocation = () => {
 };
 
 const getBrowserLocation = () => {
-  const timeout_ms = 5000;
+  const timeout_ms = 10000;
   return new Promise((resolve) => {
     if (!navigator.geolocation) {
       console.log("Geolocation not supported");
@@ -100,23 +100,23 @@ const getBrowserLocation = () => {
       resolve(state);
     } else {
       showLoadingText();
-      const fallback = setTimeout(() => {
-        if (!state.lat && !state.lon) {
-          console.log("Geolocation never returned");
-          getDefaultLocation();
-          resolve(state);
-        }
-      }, timeout_ms);
+      // const fallback = setTimeout(() => {
+      //   if (!state.lat && !state.lon) {
+      //     console.log("Geolocation never returned");
+      //     getDefaultLocation();
+      //     resolve(state);
+      //   }
+      // }, timeout_ms);
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          clearTimeout(fallback);
+          // clearTimeout(fallback);
           state.lat = position.coords.latitude;
           state.lon = position.coords.longitude;
           resolve(state);
         },
         (err) => {
           console.log("Geolocation not permitted:", err);
-          clearTimeout(fallback);
+          // clearTimeout(fallback);
           getDefaultLocation();
           resolve(state);
         },
