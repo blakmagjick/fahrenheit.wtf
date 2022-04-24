0beb15f623598234be0394241b201ab2ce74bea0
blakmagjick <suzy@iamsuzyq.com>
2022-04-23 22:43:13 -0700

    Do location query on locationInput change

--- fahrenheit.js
+++ fahrenheit.js
@@ -4,13 +4,21 @@ const state = {
   place: "",
 };
 
-const requestUrl = () => {
-  const appid = atob("MTViMDM1YTcxMzFlYjNlMGY1YzNjOTg2YjEzY2JmNTA="); // ha ha
+const appid = atob("MTViMDM1YTcxMzFlYjNlMGY1YzNjOTg2YjEzY2JmNTA="); // ha ha
+
+const weatherQuery = () => {
   return `https://api.openweathermap.org/data/2.5/weather?lat=${state.lat}&lon=${state.lon}&units=metric&appid=${appid}`;
 };
 
+const locationQuery = (search) => {
+  const q = encodeURIComponent(search);
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${q}&limit=5&appid=${appid}`;
+  console.log("locationQuery", url);
+  return url;
+};
+
 const getWeather = () => {
-  fetch(requestUrl())
+  fetch(weatherQuery())
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -20,9 +28,28 @@ const getWeather = () => {
       celsius_div.innerHTML = Math.round(temp_c) + "ºC";
       const fahr_div = document.querySelector("#fahrenheit div");
       fahr_div.innerHTML = Math.round(temp_f) + "ºF";
-      state.place = data.name;
+      if (!state.place) state.place = data.name;
       const location_div = document.querySelector("#location a");
       location_div.innerHTML = state.place;
+    });
+};
+
+const getLocationByName = () => {
+  const input = document.querySelector("#locationInput");
+  const search = input.value;
+  fetch(locationQuery(search))
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // TODO: Let users select from a dropdown, rather than just using the first one
+      const location = data[0];
+      state.lat = location.lat;
+      state.lon = location.lon;
+      state.place = location.name;
+      if (location.state) state.place += ", " + location.state;
+      if (location.country) state.place += ", " + location.country;
+      getWeather();
+      hideLocationInput();
     });
 };
 
@@ -48,7 +75,7 @@ const getDefaultLocation = () => {
   state.place = "Berkeley, CA";
 };
 
-const getLocation = () => {
+const getBrowserLocation = () => {
   return new Promise((resolve) => {
     if (!navigator.geolocation) {
       console.log("Geolocation not supported");
@@ -72,5 +99,5 @@ const getLocation = () => {
 };
 
 const pageLoaded = () => {
-  getLocation().then(() => getWeather());
+  getBrowserLocation().then(() => getWeather());
 };
